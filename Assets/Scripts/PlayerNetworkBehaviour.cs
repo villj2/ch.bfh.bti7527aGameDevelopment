327ab0ef6086ab9a2fefee684bcc0f3c5ce8127d
flec <mfuellemann@gmail.com>
2015-05-08 18:38:01 +0200

    disable opponents wheel script

--- Assets/Scripts/PlayerNetworkBehaviour.cs
+++ Assets/Scripts/PlayerNetworkBehaviour.cs
@@ -5,7 +5,12 @@ public class PlayerNetworkBehaviour : BasicNetworkBehaviour {
 
 	protected override void OnOpponentUpdate() {
 		GetComponent<CarBehaviour>().enabled = false;
-		GetComponent<Rigidbody> ().transform.Find("Turret").GetComponent<TurretBehavioiur>().enabled = false;
+		Transform trans = GetComponent<Rigidbody> ().transform;
+		trans.Find("Turret").GetComponent<TurretBehavioiur>().enabled = false;
+		trans.Find ("Wheels/FL").GetComponent<WheelBehaviour>().enabled = false;
+		trans.Find ("Wheels/FR").GetComponent<WheelBehaviour>().enabled = false;
+		trans.Find ("Wheels/RL").GetComponent<WheelBehaviour>().enabled = false;
+		trans.Find ("Wheels/RR").GetComponent<WheelBehaviour>().enabled = false;
 	}
 
 }
